6bee9085d552a2b445eaf96e11f9578f668d244b
Athisruth <Athisruthmsatheesh98@gmail.com>
2025-10-28 22:01:13 +0530

    "added"

---
+++ leet_code/random_quicksort.py
@@ -0,0 +1,18 @@
+ls = [25,2,4,1,100,5,0]
+
+
+
+def random_quicksort(ls):
+    if len(ls)<=1:
+        return ls
+    pivot = ls[len(ls)//2]
+
+    left = [x for x in ls if x<pivot]
+    middle = [x for x in ls if x==pivot]
+    right = [x for x in ls if x>pivot]
+
+    return random_quicksort(left) + middle + random_quicksort(right)
+
+print(random_quicksort(ls))
+
+
