f9835753bd1b3573b43b6ad22ef71fb04b285239
Athisruth <Athisruthmsatheesh98@gmail.com>
2025-10-28 22:16:38 +0530

    "random pivot added"

--- leet_code/quicksort.py
+++ leet_code/quicksort.py
@@ -1,9 +1,11 @@
 ls = [25,2,4,1,100,5,0]
-
+import random
 def quicksort(ls):
     if len(ls)<=1:
         return ls
     pivot = ls[len(ls)//2]
+    # pivot = random.choice(ls)#for random quicksort
+    print(pivot)
 
     left = [x for x in ls if x<pivot]
     middle = [x for x in ls if x==pivot]
