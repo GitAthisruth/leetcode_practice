66c295b0d06f4245e177236842cac8e0cf403d0c
Athisruth M Satheesh <athisruth@outlook.com>
2026-01-09 18:21:10 +0530

    "added"

--- leet_code_practice/arrays/minimum_rotated_sorted_array.py
+++ leet_code_practice/arrays/minimum_rotated_sorted_array.py
@@ -3,4 +3,16 @@
 # Explanation: The original array was [1,2,3,4,5] rotated 3 times.
 
 
-nums = [3,4,5,1,2] 
+nums = [3,4,5,0,1,2] 
+
+def minbs(nums):
+    l,r=0,len(nums)-1
+    while l<r:
+        m = (l+r)//2
+        if nums[m]>nums[r]:
+            l=m+1
+        else:
+            r=m
+    return nums[l]
+
+print(minbs(nums))
---
+++ leet_code_practice/arrays/search_in_rotated_sorted_array.py
@@ -0,0 +1,27 @@
+nums = [4,5,6,7,0,1,2]
+target = 0
+
+def bs_v(nums,target):
+    l,r=0,len(nums)-1
+    while l<=r:
+        m=(l+r)//2
+
+        if nums[m]==target:
+            return m
+        
+        if nums[l]<=nums[m]:
+            if nums[l]<=target<nums[m]:
+                r=m-1
+            else:
+                l=m+1
+
+        else:
+            if nums[m]<target<=nums[r]:
+                l=m+1
+            else:
+                r=m-1
+
+    return -1
+
+
+print(bs_v(nums,target))
