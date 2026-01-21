87e9ba5e81acb4c729a622148f4c5701c4423417
Athisruth M Satheesh <athisruth@outlook.com>
2026-01-22 02:16:31 +0530

    "added"

--- leet_code_practice/arrays/maximum_product_subarray.py
+++ leet_code_practice/arrays/maximum_product_subarray.py
@@ -1,4 +1,8 @@
-nums = [-2,0,-1]
+# Input: nums = [2,3,-2,4]
+# Output: 6
+# Explanation: [2,3] has the largest product 6.
+
+nums = [2,3,-2,4]
 current_product = nums[0]
 max_ = nums[0]
 for i in range(1,len(nums)):
