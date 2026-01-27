cf748dc92f8f7fcd99743bdecffc28fc39b2612a
Athisruth M Satheesh <athisruth@outlook.com>
2026-01-28 00:58:00 +0530

    "added"

--- leet_code_practice/matrix/word_search.py
+++ leet_code_practice/matrix/word_search.py
@@ -1,5 +1,5 @@
-board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
-word = "ABCB"
+# board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
+# word = "ABCB"
 
 
 
@@ -31,41 +31,7 @@ def word_search(board,word):
     return False
  
   
-print(word_search(board,word))
-
-
-
-
-
-def word_search(board,word):
-    w = list(word)
-    dc = {}
-    for i in board:
-        for j in i:
-            dc[j] = dc.get(j,0)+1
-
-
-    count = 0
-    ac = {}
-    for wl in w:
-        ac[wl] = ac.get(wl,0)+1
-
-    for key in ac:
-        if key in dc:
-            if dc[key] >0:
-                count+=1
-                dc[key]-=1
-
-    if count == len(word):
-        return True
-    
-    return False
-
-
-print(word_search(board,word))
-
-
-    
+# print(word_search(board,word))
 
 
 
