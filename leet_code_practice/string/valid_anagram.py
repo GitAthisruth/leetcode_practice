b2a526ef9f85ce145e609a7ba7bd92a6cde15337
Athisruth M Satheesh <athisruth@outlook.com>
2026-01-28 13:03:16 +0530

    "added"

--- leet_code_practice/string/valid_anagram.py
+++ leet_code_practice/string/valid_anagram.py
@@ -38,6 +38,36 @@ def val_ana(s,t):
 
 
 print(val_ana(s,t))
+
+
+
+
+
+
+def isAnagram(s, t):
+       
+        flag = False
+        if len(s)!=len(t):
+            return flag
+        s_d = {}
+        t_d = {}
+        for i in s:
+            s_d[i] = s_d.get(i,0)+1
+        for j in t:
+            t_d[j] = t_d.get(j,0)+1
+
+        count = 0
+        for key in s_d:
+            if key in t_d:
+                if s_d[key] == t_d[key]:
+                    count+=1
+                    if count==len(s_d):
+                        flag = True
+
+
+        return flag
     
+
+print(isAnagram(s,t))
     
      
