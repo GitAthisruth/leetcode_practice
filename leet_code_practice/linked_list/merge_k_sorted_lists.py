87bb0647e613630f22d0ec07e9f9ae4d1b9bed8b
Athisruth M Satheesh <athisruth@outlook.com>
2026-02-14 12:35:25 +0530

    added

--- leet_code_practice/linked_list/merge_k_sorted_lists.py
+++ leet_code_practice/linked_list/merge_k_sorted_lists.py
@@ -43,16 +43,13 @@ def merge_linked_list(l):
         while current:
             val.append(current.element)
             current = current.next
-    return val
+    
+    if not val:
+        return None
+    sorted_vals = splitting(val) 
+    return final_merge_ll(sorted_vals)
 
 
-
-
-
-m = merge_linked_list(l)
-
-print(m)
-p = splitting(m)
 
 def final_merge_ll(p):
     if not p:
@@ -66,8 +63,88 @@ def final_merge_ll(p):
     return head
 
 
-new_head = final_merge_ll(p)
+new_head = merge_linked_list(l)
 
 ll = LinkedList()
 ll.print_from_node(new_head)
 
+
+
+hh = [3,5,6,9,1,2,3,4,5,6,1,1]
+
+n = set()
+
+for i in hh:
+    n.add(i)
+
+print(n)
+
+
+
+
+# optimized method 
+
+
+
+
+def mergeKLists(lists):
+    """
+    :type lists: List[Optional[ListNode]]
+    :rtype: Optional[ListNode]
+    """
+    if not lists:
+        return None
+
+    while len(lists) > 1:
+        mergedLists = []
+
+        # Merge lists in pairs
+        for i in range(0, len(lists), 2):
+            l1 = lists[i]
+            l2 = lists[i + 1] if (i + 1) < len(lists) else None
+            mergedLists.append(mergeTwoLists(l1, l2))
+
+        lists = mergedLists
+        print(len(lists))
+
+    return lists[0]
+
+
+def mergeTwoLists(list1, list2):
+        # Get heads
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
+        l1 = list1.head
+        l2 = list2.head
+
+        # Dummy node
+        dummy = Node(0)
+        current = dummy
+
+        # Merge process
+        while l1 and l2:
+            if l1.element < l2.element:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+            current = current.next
+
+        # Attach remaining nodes
+        if l1:
+            current.next = l1
+        else:
+            current.next = l2
+
+        # Return as LinkedList
+        merged_list = LinkedList()
+        merged_list.head = dummy.next
+        print("Merged two lists:", merged_list.to_list())
+
+        return merged_list
+
+result = mergeKLists(l)
+result.print_list()
