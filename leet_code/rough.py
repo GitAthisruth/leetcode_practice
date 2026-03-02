9613f23b9e8ff03890640b858eaac78435c50a9a
Athisruth M Satheesh <athisruth@outlook.com>
2026-03-02 12:15:16 +0530

    "added"

--- leet_code/rough.py
+++ leet_code/rough.py
@@ -1,32 +1,12 @@
-board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
-word = "ABCDDEA"
+hh= [2,7,11,15] 
 
+def nn(hh,target=9):
+    dd = {}
+    for index,val in enumerate(hh):
+        rm = target - val
+        if rm in dd:
+            return [dd[rm],index]
+        else:
+            dd[val] = index
 
-
-
-def word_search(board,word):
-    w = list(word)
-    dc = {}
-    for i in board:
-        for j in i:
-            dc[j] = dc.get(j,0)+1
-
-
-    count = 0
-    ac = {}
-    for wl in w:
-        ac[wl] = ac.get(wl,0)+1
-
-    for key,value in ac.items():
-        if key in dc:
-            if dc[key] >0:
-                count+=1
-                dc[key]-=1
-
-    if count == len(word):
-        return True
-    
-    return False
-
-
-print(word_search(board,word))
+print(nn(hh,target=9))
